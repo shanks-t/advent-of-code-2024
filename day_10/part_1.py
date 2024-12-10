20d93e48869e5a46d17e0f9ca69177c57bc9fa2d
shanks-t <shanksleslietrey@gmail.com>
2024-12-10 11:51:16 -0800

    day 10: setup grid

--- day_10/part_1.py
+++ day_10/part_1.py
@@ -1,11 +1,15 @@
 from utils import time_it, read_input, input_path
+import numpy as np
 
 
 @time_it
 def main(data: str) -> str:
-    return data
+    array = np.fromstring(data, dtype=int, sep="\n")
+    array = np.reshape(array, (len(array), -1))
+    print(array)
+    return "fin"
 
 
 if __name__ == "__main__":
     print(main(read_input(input_path(__file__).replace(".txt", "_practice.txt"))))
-    print(main(read_input(input_path(__file__))))
+    # print(main(read_input(input_path(__file__))))
--- day_8/part_2.py
+++ day_8/part_2.py
@@ -1,10 +1,11 @@
 from utils import time_it, read_input, input_path
 import numpy as np
 from itertools import combinations
+from math import sqrt
 
 
 @time_it
-def main(data: str) -> str:
+def main(data: str) -> int:
     coordinates_dict, all_possible_points, bounding_box = preprocess_data(data)
     solution = find_intersection_of_coordinate_plane_and_antidodes(
         coordinates_dict, all_possible_points, bounding_box)
@@ -43,8 +44,8 @@ def find_intersection_of_coordinate_plane_and_antidodes(coordinates_dict: dict, 
     return len(intersection)
 
 
-def create_antinodes_set(coordinates_dict: dict, bounding_box: tuple) -> set:
-    all_antinodes_set = set()
+def create_antinodes_set(coordinates_dict: dict, bounding_box: tuple) -> set[tuple]:
+    all_antinodes_set: set[tuple] = set()
 
     for key, sattelite_points in coordinates_dict.items():
         if key not in ['.', '#']:
@@ -52,7 +53,7 @@ def create_antinodes_set(coordinates_dict: dict, bounding_box: tuple) -> set:
             combos = find_all_unique_combination_of_sattelite_points(
                 sattelite_points)
             # print(f'combos: {combos}')
-            lines_list = compute_line_from_points(combos)
+            lines_list = compute_line_from_points(key, combos)
             antinodes_list = find_all_antinode_points_in_bounding_box(
                 lines_list, bounding_box)
             # print(antinodes_list)
@@ -61,17 +62,21 @@ def create_antinodes_set(coordinates_dict: dict, bounding_box: tuple) -> set:
     return all_antinodes_set
 
 
-def find_all_antinode_points_in_bounding_box(lines_list: list[tuple[int, int]], b_box: tuple[int, int, int, int]) -> list[tuple[int, int]]:
-    points: list[tuple[int, int]] = []
+def find_all_antinode_points_in_bounding_box(lines_list: list[tuple[float, float, float]], b_box: tuple[int, int, int, int]) -> list[tuple[float, float]]:
+    points: list[tuple[float, float]] = []
     (x1, y1, x2, y2) = b_box
     for line in lines_list:
-        slope = line[0]
-        intercept = line[1]
-        for x in range(x1, x2 + 1):
-            y = slope * x + intercept
-            if y1 <= y <= y2 and y.is_integer():
-                points.append((x, y))
-    # print(f"all antinode points in bouding box: {points}")
+        A, B, C = line
+        if B != 0:  # Non-vertical line
+            for x in range(x1, x2 + 1):
+                y = -(A*x + C) / B
+                if y1 <= y <= y2 and y.is_integer():
+                    points.append((float(x), float(y)))
+        else:  # Vertical line
+            x = -C / A
+            if x1 <= x <= x2 and x.is_integer():
+                for y in range(y1, y2 + 1):
+                    points.append((float(x), float(y)))
     return points
 
 
@@ -87,19 +92,23 @@ def find_all_possible_points(coordinates_dict: dict) -> list[np.ndarray]:
     return all_possible_points
 
 
-def compute_line_from_points(combos: list[tuple]) -> list[tuple[float, float]]:
-    lines_list: list[tuple[float, float]] = []
+def compute_line_from_points(key: str, combos: list[tuple]) -> list[tuple[float, float, float]]:
+    lines_list: list[tuple[float, float, float]] = []
     for combo in combos:
         x1, y1 = np.array(combo[0])
         x2, y2 = np.array(combo[1])
-        m = (y2 - y1)/(x2-x1)
-        b = y1 - m*x1
-        lines_list.append((m, b))
+        A = y2 - y1
+        B = x1 - x2
+        C = x2*y1 - x1*y2
+        magnitude = sqrt(A**2 + B**2)
+        A, B, C = A/magnitude, B/magnitude, C/magnitude
+        lines_list.append((A, B, C))
+        # print(f"key: {key}\nlines_list: {lines_list}")
     return lines_list
 
 
 def create_coordinates_dict(data: str) -> dict[str, list[tuple[int, int]]]:
-    coordinates_dict = {}
+    coordinates_dict: dict[str, list[tuple[int, int]]] = {}
     grid = data.split("\n")
     for y, row in enumerate(reversed(grid)):
         for x, char in enumerate(row):
